456fea176c6943f30540de1df2112f32a044a4e2
Dallas Palumbo <palumbodallas@gmail.com>
2022-07-05 12:10:53 -0400

    trying

--- public/admin.js
+++ public/admin.js
@@ -1,5 +1,7 @@
 
 // Your Code Here
+let dog= document.querySelector("#dog")
+
  async function updateBook(){ 
 let response = await fetch('http://localhost:3001/updateBook',{
     method:"PATCH",
@@ -17,3 +19,33 @@ console.log(updateBook)
  }
 
  updateBook()
+
+ dog.addEventListener('click',()=>{
+    console.log("Dallas rules")
+ })
+let buttons = document.querySelectorAll('.booksSave') 
+for (let i =1; i<= buttons.length; i++){
+   console.log(buttons[i])
+    buttons[i].addEventListener('click',(i,input)=>{
+        updateCount(i,input)
+    })
+}
+console.log('hi')
+//function to update book count 
+async function updateCount(){ 
+    let response = await fetch('http://localhost:3001/updateBook',{
+        method:"PATCH",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'id':i,
+            "quantity": input.value
+        })
+    })
+    
+    let updateBook = await response.json();
+    console.log(updateBook)
+     }
+
+     let shinobi = document.querySelector('#shin')
